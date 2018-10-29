e16d143264b82ed42a1a43794128f254ec3bf75b
EC2 Default User <ec2-user@ip-172-31-27-57.us-east-2.compute.internal>
2018-10-29 02:28:11 +0000

    Add account activation

--- sample_app/config/environments/development.rb
+++ sample_app/config/environments/development.rb
@@ -27,7 +27,13 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '8ad2a462be5f46a4a32f83a0940105f2.vfs.cloud9.us-east-2.amazonaws.com' # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
